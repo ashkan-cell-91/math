5968e64eb9287f970555d794ca730264eea66714
ashkan-cell-91 <a.adabi2022@gmail.com>
2025-05-15 18:24:12 +0400

    Update settings.py

--- mokhtasat/settings.py
+++ mokhtasat/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mokhtasat.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/math',
         conn_max_age=600
     )
 }

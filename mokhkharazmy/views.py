a16c515822f5874649a11480456cef3217a02d71
ashkan-cell-91 <a.adabi2022@gmail.com>
2025-06-02 19:18:27 +0400

    Update views.py

--- mokhkharazmy/views.py
+++ mokhkharazmy/views.py
@@ -3,6 +3,26 @@ from django.contrib import messages
 from .models import Product
 from django.contrib.auth import login
 from .forms import RegisterForm
+from django.http import JsonResponse
+
+
+# داده‌های نمونه برای جستجو
+data = {
+    "python": ["آموزش مقدماتی Python", "بهترین کتاب‌های Python", "فریم‌ورک Django و Python"],
+    "javascript": ["ES6 و ویژگی‌های جدید JavaScript", "آموزش React.js", "Node.js و توسعه سمت سرور"],
+    "html": ["راهنمای کامل HTML5", "بهترین تمرین‌های طراحی وب", "CSS و طراحی پیشرفته"]
+}
+
+def search_data(request):
+    query = request.GET.get('q', '').lower()  # دریافت مقدار جستجو از URL
+    results = data.get(query, ["نتیجه‌ای یافت نشد!"])
+    
+    # اگر درخواست با هدر AJAX ارسال شده باشد، داده‌ها را به صورت JSON برگردان
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return JsonResponse({'query': query, 'results': results})
+    
+    # در غیر این صورت صفحه HTML را با نتایج جستجو رندر می‌کند.
+    return render(request, 'detector/search.html', {'results': results, 'query': query})
 
 def home(request):
     all_products = Product.objects.all()
